2583e801e71fbca74687695313a418d663da8be5
Рыбина Екатерина <rybinae58@gmail.com>
2026-05-20 15:46:32 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,39 +4,43 @@
 
 template<typename T>
 class TPQueue {
-private:
-  struct N {
-    T data;
-    N* next;
-    N(const T& val) : data(val), next(nullptr) {}
-  };
-  N* head;
-public:
-  TPQueue() : head(nullptr) {}
-  ~TPQueue() {
-    while (head != nullptr)
-      pop();
-  }
-  void push(const T& item) {
-    N* newN = new N(item);
-    if (head == nullptr || item.prior > head->data.prior) {
-      newN->next = head;
-      head = newN;
-      return;
+ private:
+    struct N {
+        T data;
+        N* next;
+        N(const T& value) : data(value), next(nullptr) {}
+    };
+    N* head;
+ public:
+
+    TPQueue() : head(nullptr) {}
+    ~TPQueue() {
+        while (head != nullptr)
+            pop();
     }
-    N* current = head;
-    while (current -> next != nullptr && current->next->data.prior >= item.prior)
-      current = current->next;
-    newN->next = current->next;
-    current->next = newN;
-  }
-  T pop() {
-    N* temp = head;
-    T res = temp->data;
-    head = head->next;
-    delete temp;
-    return res;
-  }
+
+    void push(const T& item) {
+        N* newNode = new N(item);
+        if (head == nullptr || item.prior > head->data.prior) {
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+        N* current = head;
+        while (current->next != nullptr &&
+               current->next->data.prior >= item.prior)
+            current = current->next;
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+
+    T pop() {
+        N* temp = head;
+        T res = temp->data;
+        head = head->next;
+        delete temp;
+        return res;
+    }
 };
 
 struct SYM {
